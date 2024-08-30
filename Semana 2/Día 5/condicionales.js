929186984703c536a377a9bfa0885e85bfaf5a4b
Alfredo08 <alfredosv08@gmail.com>
2024-08-30 16:00:28 -0600

    JS variables, tipos y condicionales

---
+++ Semana 2/Día 5/condicionales.js
@@ -0,0 +1,13 @@
+let totalCarrito = 1980;
+let edad = 21;
+
+
+if(totalCarrito >= 1000){
+    console.log("Has gastado más de mil. Ya cuentas con el descuento.");
+    if(edad === 21){
+        console.log("Y además como tienes 21 años, te vamos a regalar el envío.");
+    }
+}
+else{
+    console.log("Necesitas 1000 o más para ganarte el descuento o tener 21 años de edad");
+}
---
+++ Semana 2/Día 5/variablesTiposDatos.js
@@ -0,0 +1,26 @@
+
+// Comentario en línea
+
+/*
+Comentario
+en
+bloque
+*/
+
+let nombre = "Alejandro";
+let apellido = 'Martínez';
+
+let edad = 25;
+
+let graduado = true;
+
+let calificacion = null;
+
+let programa = undefined;
+
+
+console.log(nombre, apellido);
+console.log(edad);
+console.log(graduado);
+console.log(calificacion);
+console.log(programa);
